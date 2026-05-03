856d29e9fe83df0f96b2fa7cc9d99522d896dd22
mdasifhosen <mdasifhosen2028@gmail.com>
2026-05-03 21:38:27 +0600

    added e word section

---
+++ script/index.js
@@ -0,0 +1,73 @@
+
+
+const loadLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+        .then(res => res.json())
+        .then(json => {
+        displayLesson(json.data)
+    })
+}
+
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    // console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+        displayLevelWord(data.data)
+    })
+}
+
+const displayLevelWord = (words) => {
+    // console.log(words)
+    const wordContainer = document.getElementById("word-container")
+    // wordContainer.innerHTML = ""
+    
+    words.forEach(word => {
+        console.log(word)
+        const card = document.createElement('div')
+        card.innerHTML = `
+         <div class=" bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+        <h2 class="font-bold text-2xl">Eager</h2>
+        <p class="font-semibold">Meaning /Pronounciation</p>
+        <div class="font-bangla text-2xl font-medium">
+            "আগ্রহী / ইগার"
+        </div>
+        <div class="flex justify-between items-center">
+            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+
+        </div>
+
+    </div>
+        `;
+        wordContainer.append(card)
+    })
+}
+
+const displayLesson = (lessons) => {
+    
+    // 1.get the contaner & empty
+    const levelContainer = document.getElementById("level-container")
+    levelContainer.innerHTML = "";
+    // 2.get into evey lessons
+    for (let lesson of lessons) {
+        // console.log(lesson)
+      // 3.create element
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML = `
+        
+            <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+            <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
+            </button>
+        
+        `;
+        levelContainer.append(btnDiv)
+
+      // 4.append into container
+    }
+    
+
+    
+}
+loadLessons()
