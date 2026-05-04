13e46c53dff618fe3e0939390be13e47319aa335
mdasifhosen <mdasifhosen2028@gmail.com>
2026-05-04 15:50:31 +0600

    added e responsive

--- script/index.js
+++ script/index.js
@@ -3,6 +3,21 @@ const createElements = (arr) => {
     return(htmlElements.join(" "))
 }
 
+ function updateTime() {
+   const now = new Date();
+   document.getElementById("liveTime").innerText = now.toLocaleString();
+ }
+
+ setInterval(updateTime, 1000);
+
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
+
+
 const manageSpinner = (status) => {
     if (status == true) {
         document.getElementById("spinner").classList.remove("hidden")
@@ -108,7 +123,7 @@ const displayLevelWord = (words) => {
         </div>
         <div class="flex justify-between items-center">
             <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
-            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
 
         </div>
 
@@ -163,3 +178,4 @@ document.getElementById("btn-search").addEventListener('click', () => {
     })
 
 })
+
